3f44e212c475501a8e452a94e4cbb8d901b3a751
wangwang2jiajia <w.wang666@hotmail.com>
2026-08-31 06:40:15 +0000

    update bits

--- tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
+++ tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
@@ -28,7 +28,7 @@ USE ROLE ACCOUNTADMIN;
 -- Alternatively, you can use an existing warehouse in your account.
 -- =============================================================================
 
-//CREATE WAREHOUSE tasty_bytes_dbt_wh WAREHOUSE_SIZE = XLARGE;
+CREATE WAREHOUSE tasty_bytes_dbt_wh WAREHOUSE_SIZE = XLARGE;
 
 -- =============================================================================
 -- STEP 2: Create a database and schemas for integrations and model materializations
@@ -75,22 +75,22 @@ ALTER SCHEMA tasty_bytes_dbt_db.prod SET METRIC_LEVEL = 'ALL';
 -- See: https://docs.snowflake.com/en/user-guide/ui-snowsight/workspaces-git
 -- =============================================================================
 
-//USE tasty_bytes_dbt_db.integrations;
-//CREATE OR REPLACE SECRET tasty_bytes_dbt_db.integrations.tb_dbt_git_secret
-//  TYPE = password
-//  USERNAME = 'your-gh-username'
-//  PASSWORD = 'YOUR_PERSONAL_ACCESS_TOKEN';
+USE tasty_bytes_dbt_db.integrations;
+CREATE OR REPLACE SECRET tasty_bytes_dbt_db.integrations.tb_dbt_git_secret
+  TYPE = password
+  USERNAME = 'wangwang2jiajia'
+  PASSWORD = 'YOUR_PERSONAL_ACCESS_TOKEN';
 
 -- Replace 'https://github.com/my-github-account' with the URL of the GitHub
 -- account for your forked repository.
 -- This API integration is used when creating a workspace in Snowsight (Projects > Workspaces)
 -- to connect Snowflake to your forked GitHub repository.
-//CREATE OR REPLACE API INTEGRATION tb_dbt_git_api_integration
-//  API_PROVIDER = git_https_api
-//  API_ALLOWED_PREFIXES = ('https://github.com/my-github-account')
-//  -- Comment out the following line if your forked repository is public
-//  ALLOWED_AUTHENTICATION_SECRETS = (tasty_bytes_dbt_db.integrations.tb_dbt_git_secret)
-//  ENABLED = TRUE;
+CREATE OR REPLACE API INTEGRATION tb_dbt_git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/wangwang2jiajia')
+  -- Comment out the following line if your forked repository is public
+  ALLOWED_AUTHENTICATION_SECRETS = (tasty_bytes_dbt_db.integrations.tb_dbt_git_secret)
+  ENABLED = TRUE;
 
 -- =============================================================================
 -- STEP 5: (Optional) Create a network rule and external access integration
@@ -102,19 +102,19 @@ ALTER SCHEMA tasty_bytes_dbt_db.prod SET METRIC_LEVEL = 'ALL';
 -- =============================================================================
 
 -- Create NETWORK RULE for external access integration
--- CREATE OR REPLACE NETWORK RULE dbt_network_rule
---   MODE = EGRESS
---   TYPE = HOST_PORT
---   -- Minimal URL allowlist that is required for dbt deps
---   VALUE_LIST = (
---     'hub.getdbt.com',
---     'codeload.github.com'
---     );
+ CREATE OR REPLACE NETWORK RULE dbt_network_rule
+   MODE = EGRESS
+   TYPE = HOST_PORT
+   -- Minimal URL allowlist that is required for dbt deps
+   VALUE_LIST = (
+     'hub.getdbt.com',
+     'codeload.github.com'
+     );
 
 -- Create EXTERNAL ACCESS INTEGRATION for dbt access to external dbt package locations
--- CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_ext_access
---   ALLOWED_NETWORK_RULES = (dbt_network_rule)
---   ENABLED = TRUE;
+ CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_ext_access
+   ALLOWED_NETWORK_RULES = (dbt_network_rule)
+   ENABLED = TRUE;
 
 -- =============================================================================
 -- STEP 6: Set up source data - Tasty Bytes foundational data model
