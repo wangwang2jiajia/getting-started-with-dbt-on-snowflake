0d3ab7dc99f15be91af240f407c2f01c52ea4abd
wangwang2jiajia <w.wang@msn.com>
2026-09-10 04:08:07 +0000

    update bits

--- tasty_bytes_dbt_demo/schedules.sql
+++ tasty_bytes_dbt_demo/schedules.sql
@@ -3,21 +3,22 @@
 ALTER TASK IF EXISTS run_tasty_bytes_subset SUSPEND;
 ALTER TASK IF EXISTS run_tasty_bytes_full SUSPEND;
 
--- Builds a subset of the models run tests. This is an example of a subset that needs to be available early for business needs
+-- Example of a subset that needs to be available early for business needs.
+-- If tests fail here, the next task won't run
 CREATE OR ALTER TASK run_tasty_bytes_subset
-  WAREHOUSE = tasty_bytes_dbt_wh
+  WAREHOUSE = TASTY_BYTES_DBT_WH
   SCHEDULE = '12 hours'
   AS
-      execute dbt project tasty_bytes_dbt_object_gh_action args='build --select raw_customers stg_customers customers --target prod';
+   execute dbt project tasty_bytes_dbt_object_gh_action args='build --select raw_customers stg_customers customers --target prod';
 
--- Builds all models and runs tests in DAG order, failing early if any test fails
+-- Builds all models and runs tests in DAG order, failing early if any upstream test breaks
 CREATE OR ALTER TASK run_tasty_bytes_full
-  WAREHOUSE = tasty_bytes_dbt_wh
+  WAREHOUSE = TASTY_BYTES_DBT_WH
   AFTER run_tasty_bytes_subset
   AS
-      execute dbt project tasty_bytes_dbt_object_gh_action args='build --target prod';
+   execute dbt project tasty_bytes_dbt_object_gh_action args='build --target prod';
 
--- When a task is first created or if an existing task it paused, it MUST BE RESUMED to be activated
+-- When a task is first created or if an existing task is paused, it MUST BE RESUMED to be activated
 -- The tasks must be enabled in REVERSE ORDER from child to root
 ALTER TASK IF EXISTS run_tasty_bytes_full RESUME;
 ALTER TASK IF EXISTS run_tasty_bytes_subset RESUME;
